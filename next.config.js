93e3a8d9f846c022166c5e62fa32172757bf6c57
Danilo Cecilia <danilo.cecilia@zenabis.com>
2021-01-22 11:26:34 -0800

    Fix layout + Clean Code

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 
-// const withCSS = require('@zeit/next-css')
-// module.exports = withCSS({})
-// const withSass = require('@zeit/next-sass')
-// module.exports = withSass({
-//   cssModules: true,
-// })
-
 const sass = withCSS(
   withSass({
     webpack(config, options) {
@@ -27,23 +20,5 @@ const sass = withCSS(
     },
   })
 )
-
-// module.exports = withCSS(
-//   withSass({
-//     webpack(config, options) {
-//       config.module.rules.push({
-//         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-//         use: {
-//           loader: 'url-loader',
-//           options: {
-//             limit: 100000,
-//           },
-//         },
-//       })
-
-//       return config
-//     },
-//   })
-// )
 
 module.exports = withPlugins([sass, withImages])
